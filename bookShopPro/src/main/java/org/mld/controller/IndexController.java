620ad7881b4fad0185362a7f6b5fd8cae5cc9d18
mld <1323158157@qq.com>
2018-11-28 17:55:46 +0800

    second commit

---
+++ bookShopPro/src/main/java/org/mld/controller/IndexController.java
@@ -0,0 +1,15 @@
+package org.mld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mld
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/login")
+    public void index(){
+
+    }
+}
--- bookShopPro/src/main/java/org/mld/controller/LoginController.java
+++ bookShopPro/src/main/java/org/mld/controller/LoginController.java
@@ -1,4 +1,19 @@
 package org.mld.controller;
 
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author mld
+ */
+@Controller
 public class LoginController {
+    public String index(HttpSession session){
+        if(session.getAttribute("user")!=null){
+            return "redirect:"+session.getAttribute("requestParentPath");
+        }else{
+            return "redirect:login/login";
+        }
+    }
 }
